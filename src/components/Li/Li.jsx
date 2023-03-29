f9fb2e1743cfefe326badd76fbad6c0593e475bd
SM Sohag <sohagsheik32@gmail.com>
2023-03-29 23:51:13 +0600

    update

--- src/components/Li/Li.jsx
+++ src/components/Li/Li.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 const Li = (props) => {
   const item = props.item;
+  const name = item.strDrink;
   return (
     <div>
-      <li className="bg-green-200">{item.strDrink}</li>
+      <li className="bg-green-200">{name}</li>
     </div>
   );
 };
