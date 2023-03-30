40b2b3f5a5d860214a56dee942b8fa4c868cbcef
SM Sohag <sohagsheik32@gmail.com>
2023-03-30 18:53:48 +0600

    some changing

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 
 const Banner = () => {
+  const article = [
+    "Hello Welcome to mirror",
+    "Typing something",
+    "Do anything diffarents",
+  ];
+  const articleIndex = 0;
+  const ar = setInterval(() => {
+    return article[articleIndex];
+    console.log(ar);
+  }, 1500);
   return (
     <div className="carousel w-full h-[450px]">
       <div id="slide1" className="carousel-item relative w-full">
@@ -39,10 +49,7 @@ const Banner = () => {
       <div id="slide4" className="carousel-item relative w-full">
         <img src="./blog2.png" className="w-full opacity-40" />
         <p className="absolute top-[50%] px-60 bg-opacity-60 text-black">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam
-          accusamus deleniti exercitationem totam quibusdam eos quisquam, quam
-          odio ipsum laboriosam! Nobis deserunt temporibus earum animi magnam
-          rerum, tenetur nemo odit.
+          {ar}
         </p>
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide3" className="btn btn-circle">
